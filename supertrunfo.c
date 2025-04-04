9ae06ca074e69dbcafc14afdf0b534a7eaa9f9d4
Felipe Lima <felipelimabox@gmail.com>
2025-04-04 22:17:41 +0000

    add print código

---
+++ supertrunfo.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+   
+    int main(){
+        
+        //Variáveis_cidade01:
+        char estado01 = "A";
+        char codigo01[60] = "A01";
+        char cidade01[60] = "São Leopoldo";
+        int populacao01 = 225.669;
+        float area01 = 102.740;
+        int pib01 = 11;
+        int turismo01 = 15;
+
+        //Variáveis_cidade02:
+        char estado02 = "B";
+        char codigo02[60] = "B02";
+        char cidade02[60] = "Porto Alegre";
+        float populacao02 = 1.390;
+        float area02 = 496.83;
+        float pib02 = 76.070;
+        int turismo02 = 25;
+
+        //Cadastro_carta01:
+        printf("Carta nº1: \n");
+        printf("Estado: %c \n", estado01);
+        printf("Código: %s \n", codigo01);
+
+        return 0;
+
+
+
+
+
+
+
+
+
+}
