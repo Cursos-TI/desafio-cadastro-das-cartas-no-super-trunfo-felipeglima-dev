3c3ce3e4dc6df1d6edeb5cb920ede9a000da28cc
Felipe Lima <felipelimabox@gmail.com>
2025-04-05 20:31:12 +0000

    add de variáveis cidade03

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,27 +3,43 @@
     int main(){
         
         //Variáveis_cidade01:
-        char estado01 = "A";
-        char codigo01[60] = "A01";
-        char cidade01[60] = "São Leopoldo";
-        int populacao01 = 225.669;
-        float area01 = 102.740;
-        int pib01 = 11;
-        int turismo01 = 15;
+        char estado01;
+        char codigo01[60];
+        char cidade01[60];
+        int populacao01;
+        float area01;
+        int pib01;
+        int nturismo01;
 
         //Variáveis_cidade02:
-        char estado02 = "B";
-        char codigo02[60] = "B02";
-        char cidade02[60] = "Porto Alegre";
-        float populacao02 = 1.390;
-        float area02 = 496.83;
-        float pib02 = 76.070;
-        int turismo02 = 25;
+        char estado02;
+        char codigo02[60];
+        char cidade02[60];
+        float populacao02;
+        float area02;
+        float pib02;
+        int turismo02;
+
+        //Variáveis_cidade03:
+        char estado03;
+        char codigo03[60];
+        char cidade03[60];
+        float populacao03;
+        float area03;
+        float pib03;
+        int turismo03;
 
         //Cadastro_carta01:
         printf("Carta nº1: \n");
-        printf("Estado: %c \n", estado01);
-        printf("Código: %s \n", codigo01);
+        printf("Digite a sua idade: ");
+        scanf("%d", &idade);   
+        printf("A idade é: %d\n", idade);
+        printf("Digite a sua altura: ");
+        scanf("%f", &altura);
+        printf("Sua altura é: %.2f\n", altura);
+        printf("Digite seu nome: ");
+        scanf("%s", &nome);
+        printf("Seu nome é: %s\n", nome);
 
         return 0;
 
