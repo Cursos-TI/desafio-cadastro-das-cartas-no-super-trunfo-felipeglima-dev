c87c7ff22c543763771ee4a221d96efc531387bc
Felipe Lima <felipelimabox@gmail.com>
2025-04-05 21:05:52 +0000

    Correção linha 41 - scanfcidade01

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,27 +3,27 @@
     int main(){
         
         //Variáveis_cidade01:
-        char estado01;
+        char estado01[20];
         char codigo01[60];
-        char cidade01[60];
+        char cidade01[100];
         int populacao01;
         float area01;
         int pib01;
         int nturismo01;
 
         //Variáveis_cidade02:
-        char estado02;
+        char estado02[20];
         char codigo02[60];
-        char cidade02[60];
+        char cidade02[100];
         float populacao02;
         float area02;
         float pib02;
         int turismo02;
 
         //Variáveis_cidade03:
-        char estado03;
+        char estado03[20];
         char codigo03[60];
-        char cidade03[60];
+        char cidade03[100];
         float populacao03;
         float area03;
         float pib03;
@@ -31,15 +31,15 @@
 
         //Cadastro_carta01:
         printf("Carta nº1: \n");
-        printf("Digite o Estado : \n");
+        printf("Digite o Código do Estado: ");
         scanf("%s", &estado01);   
-        printf("Estado : %s\n", estado01);
+        printf("Estado: %s\n", estado01);
         printf("Digite o Código da Carta: ");
         scanf("%s", &codigo01);
-        printf("Código da Carta : %s\n", codigo01);
-        printf("Digite o nome da cidade : \n");
+        printf("Código da Carta: %s\n", codigo01);
+        printf("Digite o nome da cidade: ");
         scanf("%s", &cidade01);
-        printf("Cidade : %s\n", cidade01);
+        printf("Cidade: %s\n", cidade01);
 
         return 0;
 
