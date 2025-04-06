bf1c2545acb566132d5485b41e8ad196ce173805
Felipe Lima <felipelimabox@gmail.com>
2025-04-06 16:14:33 +0000

    add cadastro carta02

--- supertrunfo.c
+++ supertrunfo.c
@@ -14,6 +14,7 @@
 
         //Cadastro_carta01:
         printf("Carta nº1: \n");
+        printf("\n");
 
         printf("Insira o Código do Estado: ");
         scanf("%s", &estado01);   
@@ -43,6 +44,47 @@
         scanf("%d", &nturismo01);
         printf("São %d pontos turísticos. \n", nturismo01);
 
+        //Variáveis_cidade02:
+        char estado02[20];
+        char codigo02[20];
+        char cidade02[100];
+        int populacao02;
+        float area02;
+        float pib02;
+        int nturismo02;
+
+
+        //Cadastro_carta02:
+        printf("Carta nº2: \n");
+        printf("\n");
+
+        printf("Insira o Código do Estado: ");
+        scanf("%s", &estado02);   
+        printf("Estado: %s\n", estado02);
+
+        printf("Insira o Código da Carta: ");
+        scanf("%s", &codigo02);
+        printf("Código da Carta: %s\n", codigo02);
+
+        printf("Insira o nome da cidade: ");
+        scanf("%s", &cidade02);
+        printf("Cidade: %s\n", cidade02);
+
+        printf("Insira o Nº Populacional: ");
+        scanf("%d", &populacao02);
+        printf("População: %d habitantes \n", populacao02);
+
+        printf("Insira a área total da cidade: ");
+        scanf( "%f", &area02);
+        printf("Área total %.3f Km². \n", area02);
+
+        printf("Insira o PIB referente: ");
+        scanf("%f", &pib02);
+        printf("PIB: %.3f Bilhões de Reais \n", pib02);
+
+        printf("Nº de Pontos Turísticos? ");
+        scanf("%d", &nturismo02);
+        printf("São %d pontos turísticos. \n", nturismo02);
 
         return 0;
 
