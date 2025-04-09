b41b148c3ebe5ae1872c6a2dfac84052dcc1e976
Felipe Lima <felipelimabox@gmail.com>
2025-04-09 21:11:17 +0000

    Erro em Super Poder continua. Overflow talvez.

--- supertrunfo.c
+++ supertrunfo.c
@@ -19,8 +19,9 @@
         pib_per_capita1 = pib1 / populacao1;
         float inverso_densidade_populacional1;
         inverso_densidade_populacional1 = 1 / densidade_populacional1;
-        float super_poder1;
-        super_poder1 = populacao1 + area1 + pib1 + (float)nturismo1 + pib_per_capita1 + inverso_densidade_populacional1; 
+        unsigned long int super_poder1;
+        super_poder1 = (int) populacao1 + (int) area1 + (int) pib1 + nturismo1 + (int) 
+        pib_per_capita1 + (int) inverso_densidade_populacional1; 
 
 
         //CARTA01:
@@ -62,13 +63,13 @@
             printf("Nome do Estado: %s\n", estado1);
             printf("Código da Carta: %s\n", codigo1);
             printf("Cidade: %s\n", cidade1);
-            printf("População: %.3f habitantes \n", populacao1);
+            printf("População: %.3f Habitantes \n", populacao1);
             printf("Área Total %.3f Km². \n", area1);
             printf("PIB: %.3f Bilhões de Reais \n", pib1);
             printf("São %d pontos turísticos. \n", nturismo1);
             printf("Densidade Populacional: %.3f hab/km²\n", populacao1 / area1);
             printf("PIB per Capita: %.3f reais\n", pib1 / populacao1);
-            printf("Super Poder: %f pontos\n", super_poder1);
+            printf("Super Poder: %lu pontos\n", (float) super_poder1);
             printf("Computando..... ");
             printf("CARTA (01) CADASTRADA COM SUCESSO!\n");
             printf("\n");
