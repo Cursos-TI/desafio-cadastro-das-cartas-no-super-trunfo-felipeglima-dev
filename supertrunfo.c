1a4c666a8378067cfe78b0fef9f230a7dd7a81b5
Felipe Lima <felipelimabox@gmail.com>
2025-04-11 17:48:50 +0000

    add variáveis e funções carta02

--- supertrunfo.c
+++ supertrunfo.c
@@ -51,7 +51,6 @@
 
         densidade_populacional1 = populacao1 / area1;
         pib_per_capita1 = pib1 / populacao1;
-        inverso_densidade_populacional1;
         inverso_densidade_populacional1 = 1 / densidade_populacional1;
         superpoder_1 = populacao1 + area1 + pib1 + (float) nturismo1 +
         pib_per_capita1 + inverso_densidade_populacional1;
@@ -75,6 +74,79 @@
             printf("CARTA (01) CADASTRADA COM SUCESSO!\n");
             printf("\n");
             printf("-----------------------------------\n");
+
+    //VARIÁVEIS_02:
+
+    char codigo_estado2[90];
+    char estado2[90];
+    char codigo_cidade2[90];
+    char cidade2[100];
+    float populacao2, area2, pib2;
+    int nturismo2;
+    float densidade_populacional2, pib_per_capita2, inverso_densidade_populacional2;
+    double superpoder_2;
+
+
+    //ENTRADA DE DADOS_02:
+
+    printf("\n");
+    printf("CADASTRE SUA 2º CARTA(01)! \n");
+    printf("\n");
+
+    printf("Insira o código do Estado: ");
+    scanf(" %[^\n]", codigo_estado2);
+
+    printf("Insira o Nome do Estado: ");
+    scanf(" %[^\n]", estado2);
+
+    printf("Insira o Código da Carta: "); 
+    scanf(" %[^\n]", codigo_cidade2);
+
+    printf("Insira o nome da cidade: "); 
+    scanf(" %[^\n]", cidade2);
+
+    printf("Insira o Nº Populacional: "); 
+    scanf("%f", &populacao2);
+
+    printf("Insira a Área Total da cidade: ");
+    scanf( "%f", &area2);
+
+    printf("Insira o PIB referente: ");
+    scanf("%f", &pib2);
+
+    printf("Nº de Pontos Turísticos: ");
+    scanf("%d", &nturismo2);
+
+    //CÁLCULOS_02:
+
+    densidade_populacional2 = populacao2 / area2;
+    pib_per_capita2 = pib2 / populacao2;
+    inverso_densidade_populacional2 = 1 / densidade_populacional2;
+    superpoder_2 = populacao2 + area2 + pib2 + (float) nturismo2 +
+    pib_per_capita2 + inverso_densidade_populacional2;
+
+
+    //SAÍDA DE DADOS_02:
+
+        printf("Código do Estado: %s\n", codigo_estado2);
+        printf("Nome do Estado: %s\n", estado2);
+        printf("Código da Carta: %s\n", codigo_cidade2);
+        printf("Cidade: %s\n", cidade2);
+        printf("População: %.3f Habitantes \n", populacao2);
+        printf("Área Total %.3f Km². \n", area2);
+        printf("PIB: %.3f Bilhões de Reais \n", pib2);
+        printf("São %d pontos turísticos. \n", nturismo2);
+        printf("Densidade Populacional: %.3f hab/km²\n", populacao2 / area2);
+        printf("PIB per Capita: %.3f reais\n", pib1 / populacao2);
+        printf ("Inverso Densidade Populacional: %.3f\n", inverso_densidade_populacional2);
+        printf("Super Poder: %.3f pontos\n", superpoder_2);
+        printf("Computando..... ");
+        printf("CARTA (02) CADASTRADA COM SUCESSO!\n");
+        printf("\n");
+        printf("-----------------CARTAS CADATRADAS------------------\n");
+
+
+
 
 return 0;
 
